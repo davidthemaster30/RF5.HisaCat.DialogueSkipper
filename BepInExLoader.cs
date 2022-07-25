02c91745dc9f5812271be9e392ecd40f3284f4f1
HisaCat <ahisacat@gmail.com>
2022-07-26 00:09:45 +0900

    v1.0.2

--- BepInExLoader.cs
+++ BepInExLoader.cs
@@ -15,7 +15,7 @@ namespace RF5.HisaCat.DialogueSkipper
             MODNAME = "DialogueSkipper",
             AUTHOR = "HisaCat",
             GUID = "RF5." + AUTHOR + "." + MODNAME,
-            VERSION = "1.0.1";
+            VERSION = "1.0.2";
 
         public static BepInEx.Logging.ManualLogSource log;
 
