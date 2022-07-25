2d1ee2a28066cecec23ce2676e20b83180a540c1
HisaCat <ahisacat@gmail.com>
2022-07-25 20:59:41 +0900

    Set version to v1.0.0

--- BepInExLoader.cs
+++ BepInExLoader.cs
@@ -15,7 +15,7 @@ namespace RF5.HisaCat.DialogueSkipper
             MODNAME = "DialogueSkipper",
             AUTHOR = "HisaCat",
             GUID = "RF5." + AUTHOR + "." + MODNAME,
-            VERSION = "1.0.0.0";
+            VERSION = "1.0.0";
 
         public static BepInEx.Logging.ManualLogSource log;
 
