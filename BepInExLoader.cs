3c2afe7bc8c82c7227c3aa77987ea4c0179ab55b
David <davidthemaster30@hotmail.com>
2024-12-01 01:26:57 -0500

    Migrate to config manager compatable build

--- BepInExLoader.cs
+++ BepInExLoader.cs
@@ -1,10 +1,7 @@
 ﻿using BepInEx;
-using BepInEx.Configuration;
 using HarmonyLib;
 using BepInEx.Unity.IL2CPP;
 using Il2CppInterop.Runtime.Injection;
-using BepInEx.Unity.IL2CPP.UnityEngine;
-
 
 namespace RF5.HisaCat.DialogueSkipper;
 
@@ -17,74 +14,7 @@ public class BepInExLoader : BasePlugin
         GUID = "RF5." + AUTHOR + "." + MODNAME,
         VERSION = "1.0.2";
 
-    public static BepInEx.Logging.ManualLogSource log;
-
-    public static ConfigEntry<bool> bDevLog;
-    public static ConfigEntry<float> fSkipDelayTimeSec;
-    public static ConfigEntry<string> shortCutConfig;
-    public static List<KeyCode> shortCutKeys = null;
-    public static ConfigEntry<string> shortCutRF5KeyConfig;
-    public static RF5Input.Key shortCutRF5Key = 0;
-
-    public BepInExLoader()
-    {
-        log = Log;
-
-        bDevLog = Config.Bind("Options", "Print dev log", false);
-        fSkipDelayTimeSec = Config.Bind("Options", "Skip Delay Time Sec", 0f,
-            new ConfigDescription("Delay time in seconds between dialogues on skip"));
-
-        {
-            shortCutConfig = Config.Bind("Keys",
-                "Skip Dialogue (KeyCode)",
-                string.Join(" | ", new KeyCode[] { KeyCode.LeftControl }.Select(x => x.ToString())),
-                new ConfigDescription("UnityEngine.KeyCode sets for skip dialogue (Combination with OR \'|\')" + Environment.NewLine + 
-                "See KeyCodes at https://docs.bepinex.dev/master/api/BepInEx.IL2CPP.UnityEngine.KeyCode.html"));
-            shortCutKeys = new List<KeyCode>();
-            {
-                var keysStr = shortCutConfig.Value.Split('|').Select(x => x.Replace(" ", ""));
-                foreach (var keyStr in keysStr)
-                {
-                    KeyCode key;
-                    if (Enum.TryParse(keyStr, out key))
-                    {
-                        if (shortCutKeys.Contains(key) == false){
-                            shortCutKeys.Add(key);
-                        }
-                    }
-                }
-            }
-            shortCutConfig.Value = string.Join(" | ", shortCutKeys.Select(x => x.ToString()));
-        }
-
-        {
-            shortCutRF5KeyConfig = Config.Bind("Keys",
-                "Skip Dialogue (RF5Input.Key)",
-                string.Join(" | ", new RF5Input.Key[] { RF5Input.Key.R }.Select(x => x.ToString())),
-                new ConfigDescription("RF5Input.Key for skip dialogue (Combination with OR \'|\')" + Environment.NewLine + 
-                "See Key at https://gist.github.com/hisacat/612a47466cc6ab66f87bc7a677c5cfb7"));
-            var temp = new List<RF5Input.Key>();
-            {
-                var keysStr = shortCutRF5KeyConfig.Value.Split('|').Select(x => x.Replace(" ", ""));
-                foreach (var keyStr in keysStr)
-                {
-                    RF5Input.Key key;
-                    if (Enum.TryParse(keyStr, out key))
-                    {
-                        if (temp.Contains(key) == false){
-                            temp.Add(key);
-                        }
-                    }
-                }
-            }
-            shortCutRF5KeyConfig.Value = string.Join(" | ", temp.Select(x => x.ToString()));
-
-            foreach (var key in temp)
-            {
-                shortCutRF5Key |= key;
-            }
-        }
-    }
+    public static BepInEx.Logging.ManualLogSource log = BepInEx.Logging.Logger.CreateLogSource("DialogueSkipper");
 
     public override void Load()
     {
@@ -100,6 +30,8 @@ public class BepInExLoader : BasePlugin
 
         try
         {
+            DialogueSkipper.LoadConfig(Config);
+
             var harmony = new Harmony(GUID);
 
             var originalUpdate = AccessTools.Method(typeof(UnityEngine.UI.CanvasScaler), "Update");
--- DialogueSkipper.cs
+++ DialogueSkipper.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using BepInEx.Unity.IL2CPP.UnityEngine;
+//using BepInEx.Unity.IL2CPP.UnityEngine;
+using BepInEx.Configuration;
 
 namespace RF5.HisaCat.DialogueSkipper;
 
@@ -12,100 +13,116 @@ public class DialogueSkipper : MonoBehaviour
 
     private int lastAddr = 0;
     private float curSkipWaitTime = 0;
+
+    public static ConfigEntry<bool> PrintDevLog;
+    public static ConfigEntry<float> SkipDelayTimeSec;
+    public static ConfigEntry<KeyboardShortcut> shortCutKey;
+    public static ConfigEntry<uint> shortCutRF5Key;
+    private static readonly uint[] ButtonList = new List<uint>((uint[])Enum.GetValues(typeof(RF5Input.Key))).Where(x => x != (uint)RF5Input.Key.ANYKEY || x != (uint)RF5Input.Key.ALLKEY).ToArray();
+    private static readonly ConfigDescription shortCutRF5KeyDescription = new ConfigDescription(
+    $"RF5Input.Key for skip dialogue.{Environment.NewLine}See Keys at https://gist.github.com/hisacat/612a47466cc6ab66f87bc7a677c5cfb7",
+    new AcceptableValueList<uint>(ButtonList));
+
+    internal static void LoadConfig(ConfigFile Config)
+    {
+        PrintDevLog = Config.Bind("Options", nameof(PrintDevLog), false);
+        SkipDelayTimeSec = Config.Bind("Options", nameof(SkipDelayTimeSec), 0f, new ConfigDescription("Delay time in seconds between dialogues on skip"));
+        shortCutKey = Config.Bind("Keys", "Skip Dialogue (KeyCode)", new KeyboardShortcut(KeyCode.LeftControl), $"UnityEngine.KeyCode sets for skip dialogue{Environment.NewLine}See KeyCodes at https://docs.bepinex.dev/master/api/BepInEx.IL2CPP.UnityEngine.KeyCode.html");
+        shortCutRF5Key = Config.Bind("Keys", "Skip Dialogue (RF5Input.Key)", (uint)RF5Input.Key.R, shortCutRF5KeyDescription);
+    }
+
     public void Update()
     {
-        if (AdvMain.Instance is not null)
+        if (AdvMain.Instance is null)
         {
-            if (AdvMain.Instance.scriptwork is not null)
+            return;
+        }
+
+        if (AdvMain.Instance.scriptwork is not null)
+        {
+            if (lastAddr != AdvMain.Instance.scriptwork.Addr)
             {
-                if (lastAddr != AdvMain.Instance.scriptwork.Addr)
+                lastAddr = AdvMain.Instance.scriptwork.Addr;
+                curSkipWaitTime = 0;
+            }
+        }
+        else
+        {
+            lastAddr = 0;
+        }
+
+        bool execute = false;
+
+        if (Event.current is not null && (shortCutKey.Value.IsDown() || shortCutKey.Value.IsPressed() || RF5Input.Pad.Edge((RF5Input.Key)shortCutRF5Key.Value)))
+        {
+            execute = true;
+        }
+
+        if (execute)
+        {
+            if (PrintDevLog.Value)
+            {
+                BepInExLoader.log.LogMessage($"[DialogueSkipper] Print...");
+                BepInExLoader.log.LogMessage($"SelectMenuCount: {AdvMain.Instance.SelectMenuCount}");
+                BepInExLoader.log.LogMessage($"SelectMenuTotalCount: {AdvMain.Instance.SelectMenuTotalCount}");
+                BepInExLoader.log.LogMessage($"work: {AdvMain.Instance.work}");
+                BepInExLoader.log.LogMessage($"nextWork: {AdvMain.Instance.nextWork}");
+                BepInExLoader.log.LogMessage($"skipable: {AdvMain.Instance.skipable}");
+                BepInExLoader.log.LogMessage($"isWait: {AdvMain.Instance.isWait}");
+                BepInExLoader.log.LogMessage($"waitSec: {AdvMain.Instance.waitSec}");
+                BepInExLoader.log.LogMessage($"waitStartTime: {AdvMain.Instance.waitStartTime}");
+                BepInExLoader.log.LogMessage($"IsTimelineEndWait: {AdvMain.Instance.IsTimelineEndWait}");
+                BepInExLoader.log.LogMessage($"GetCurrentScriptName(): {AdvMain.Instance.GetCurrentScriptName()}");
+                BepInExLoader.log.LogMessage($"scriptwork.Addr: {AdvMain.Instance.scriptwork?.Addr}");
+                BepInExLoader.log.LogMessage($"scriptwork.commandIndex: {AdvMain.Instance.scriptwork?.commandIndex}");
+                BepInExLoader.log.LogMessage($"scriptwork.commandNum: {AdvMain.Instance.scriptwork?.commandNum}");
+
+                BepInExLoader.log.LogMessage($"this.curSkipWaitTime: {curSkipWaitTime}");
+            }
+
+            try
+            {
+                switch (AdvMain.Instance.work)
                 {
-                    lastAddr = AdvMain.Instance.scriptwork.Addr;
-                    curSkipWaitTime = 0;
-                }
-            }
-            else
-            {
-                lastAddr = 0;
-            }
-
-            bool execute = false;
-            if (Event.current is not null && BepInExLoader.shortCutKeys.All(x => Input.GetKeyInt(x)))
-            {
-                execute = true;
-            }
-
-            if (!execute && BepInExLoader.shortCutRF5Key != 0 && ((RF5Input.Pad.Data.PushData & BepInExLoader.shortCutRF5Key) == BepInExLoader.shortCutRF5Key))
-            {
-                execute = true;
-            }
-
-            if (execute)
-            {
-                if (BepInExLoader.bDevLog.Value)
-                {
-                    BepInExLoader.log.LogMessage($"[DialogueSkipper] Print...");
-                    BepInExLoader.log.LogMessage($"SelectMenuCount: {AdvMain.Instance.SelectMenuCount}");
-                    BepInExLoader.log.LogMessage($"SelectMenuTotalCount: {AdvMain.Instance.SelectMenuTotalCount}");
-                    BepInExLoader.log.LogMessage($"work: {AdvMain.Instance.work}");
-                    BepInExLoader.log.LogMessage($"nextWork: {AdvMain.Instance.nextWork}");
-                    BepInExLoader.log.LogMessage($"skipable: {AdvMain.Instance.skipable}");
-                    BepInExLoader.log.LogMessage($"isWait: {AdvMain.Instance.isWait}");
-                    BepInExLoader.log.LogMessage($"waitSec: {AdvMain.Instance.waitSec}");
-                    BepInExLoader.log.LogMessage($"waitStartTime: {AdvMain.Instance.waitStartTime}");
-                    BepInExLoader.log.LogMessage($"IsTimelineEndWait: {AdvMain.Instance.IsTimelineEndWait}");
-                    BepInExLoader.log.LogMessage($"GetCurrentScriptName(): {AdvMain.Instance.GetCurrentScriptName()}");
-                    BepInExLoader.log.LogMessage($"scriptwork.Addr: {AdvMain.Instance.scriptwork.Addr}");
-                    BepInExLoader.log.LogMessage($"scriptwork.commandIndex: {AdvMain.Instance.scriptwork.commandIndex}");
-                    BepInExLoader.log.LogMessage($"scriptwork.commandNum: {AdvMain.Instance.scriptwork.commandNum}");
-
-                    BepInExLoader.log.LogMessage($"this.curSkipWaitTime: {curSkipWaitTime}");
-                }
-
-                try
-                {
-                    switch (AdvMain.Instance.work)
-                    {
-                        case AdvMain.WorkList.WORK_NONE:
-                            break;
-                        case AdvMain.WorkList.WORK_MESSAGE_WAIT:
+                    case AdvMain.WorkList.WORK_NONE:
+                        break;
+                    case AdvMain.WorkList.WORK_MESSAGE_WAIT:
+                        {
+                            //Print text to end.
+                            if (AdvMain.Instance.textWindow?.textLength > 0 && AdvMain.Instance.textWindow.dispLength < AdvMain.Instance.textWindow.textLength)
                             {
-                                //Print text to end.
-                                if (AdvMain.Instance.textWindow?.textLength > 0 && AdvMain.Instance.textWindow.dispLength < AdvMain.Instance.textWindow.textLength)
-                                {
-                                    AdvMain.Instance.textWindow.forceDisp();
-                                }
+                                AdvMain.Instance.textWindow.forceDisp();
+                            }
 
-                                if (curSkipWaitTime >= BepInExLoader.fSkipDelayTimeSec.Value)
-                                {
-                                    //Simulate click TextWindow.
-                                    AdvMain.Instance.onTextWindowClick();
-                                }
-                            }
-                            break;
-                        case AdvMain.WorkList.WORK_SELECT_WAIT:
-                            break;
-                        case AdvMain.WorkList.WORK_TIMELINE_END_WAIT:
+                            if (curSkipWaitTime >= SkipDelayTimeSec.Value)
                             {
-                                //Basically it can skip with press 'F' key
+                                //Simulate click TextWindow.
+                                AdvMain.Instance.onTextWindowClick();
                             }
-                            break;
-                        case AdvMain.WorkList.WORK_WAIT:
-                            {
-                                AdvMain.Instance.waitStartTime = AdvMain.Instance.waitSec;
-                            }
-                            break;
-                    }
+                        }
+                        break;
+                    case AdvMain.WorkList.WORK_SELECT_WAIT:
+                        break;
+                    case AdvMain.WorkList.WORK_TIMELINE_END_WAIT:
+                        {
+                            //Basically it can skip with press 'F' key
+                        }
+                        break;
+                    case AdvMain.WorkList.WORK_WAIT:
+                        {
+                            AdvMain.Instance.waitStartTime = AdvMain.Instance.waitSec;
+                        }
+                        break;
                 }
-                catch (Exception e)
-                {
-                    BepInExLoader.log.LogError($"[DialogueSkipper] Exception: {e}");
-                }
+            }
+            catch (Exception e)
+            {
+                BepInExLoader.log.LogError($"[DialogueSkipper] Exception: {e}");
+            }
 
-                if (BepInExLoader.bDevLog.Value)
-                {
-                    BepInExLoader.log.LogMessage($"[DialogueSkipper] Print ends.");
-                }
+            if (PrintDevLog.Value)
+            {
+                BepInExLoader.log.LogMessage($"[DialogueSkipper] Print ends.");
             }
         }
 
