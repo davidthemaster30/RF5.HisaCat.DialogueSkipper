a66eaeeee38e2d27c3489dde2d88f550d87a38f7
David <davidthemaster30@hotmail.com>
2024-12-01 01:17:45 -0500

    return early

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -12,38 +12,40 @@ public class Bootstrapper : MonoBehaviour
     [HarmonyPostfix]
     public static void Update()
     {
-        if (instance_DialogueSkipper is null)
+        if (instance_DialogueSkipper is not null)
         {
-            BepInExLoader.log.LogMessage("[DialogueSkipper] Initializing...");
-            GameObject? containerObj = null;
-            try
-            {
-                containerObj = new GameObject("#DialogueSkipper#");
-                DontDestroyOnLoad(containerObj);
-                instance_DialogueSkipper = containerObj.AddComponent<DialogueSkipper>();
+            return;
+        }
 
-                if (instance_DialogueSkipper is not null)
-                {
-                    BepInExLoader.log.LogMessage("[DialogueSkipper] DialogueSkipper created!");
-                }
-                else
-                {
-                    if (containerObj is not null)
-                    {
-                        Destroy(containerObj);
-                        containerObj = null;
-                    }
-                }
+        BepInExLoader.log.LogMessage("[DialogueSkipper] Initializing...");
+        GameObject? containerObj = null;
+        try
+        {
+            containerObj = new GameObject("#DialogueSkipper#");
+            DontDestroyOnLoad(containerObj);
+            instance_DialogueSkipper = containerObj.AddComponent<DialogueSkipper>();
+
+            if (instance_DialogueSkipper is not null)
+            {
+                BepInExLoader.log.LogMessage("[DialogueSkipper] DialogueSkipper created!");
             }
-            catch (Exception e)
+            else
             {
-                BepInExLoader.log.LogMessage($"[DialogueSkipper] Initializing failed. {e}");
-
                 if (containerObj is not null)
                 {
                     Destroy(containerObj);
                     containerObj = null;
                 }
+            }
+        }
+        catch (Exception e)
+        {
+            BepInExLoader.log.LogMessage($"[DialogueSkipper] Initializing failed. {e}");
+
+            if (containerObj is not null)
+            {
+                Destroy(containerObj);
+                containerObj = null;
             }
         }
     }
