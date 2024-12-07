f198dafdbd148faa2317016edbb5eecf6a3dec33
David <davidthemaster30@hotmail.com>
2024-12-07 13:00:57 -0500

    add pdb to get better error resolution

--- make_release.sh
+++ make_release.sh
@@ -3,4 +3,4 @@ rm -rf bin
 
 dotnet build RF5.HisaCat.DialogueSkipper.csproj -f net6.0 -c Release
 
-zip -j 'RF5.HisaCat.DialogueSkipper_v1.1.0.zip' './bin/Release/net6.0/RF5.HisaCat.DialogueSkipper.dll'
+zip -j 'RF5.HisaCat.DialogueSkipper_v1.1.1.zip' './bin/Release/net6.0/RF5.HisaCat.DialogueSkipper.dll' './bin/Release/net6.0/RF5.HisaCat.DialogueSkipper.pdb'
